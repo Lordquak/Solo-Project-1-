6285fbbd24709e22b5f74399b0ca0d964517747f
Josh <jproano2023@Elam.co.uk>
2025-02-12 11:49:16 +0000

    gfg

--- Assets/Scripts/Interactor.cs
+++ Assets/Scripts/Interactor.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Wallrunning : MonoBehaviour
+public class Interactor : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
