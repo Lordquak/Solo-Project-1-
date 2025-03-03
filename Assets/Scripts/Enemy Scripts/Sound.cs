e10fa40916417748a23968e61f08f19fe0049c28
Josh <jproano2023@Elam.co.uk>
2025-03-03 12:42:02 +0000

    cave tests and sound detection start

--- Assets/Scripts/Enemy Scripts/Sound.cs
+++ Assets/Scripts/Enemy Scripts/Sound.cs
@@ -2,8 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sound : MonoBehaviour
+namespace GamePlay
 {
-    
-    
+    public class Sound
+    {
+        public Sound(Vector3 _pos, float _range)
+        {
+            pos = _pos;
+
+            range = _range;
+
+
+        }
+
+        public readonly Vector3 pos;
+
+        public readonly float range;
+    }
 }
+ 
