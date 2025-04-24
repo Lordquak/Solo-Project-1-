2bf8c86154f04311de6a61dca940d6f35dd72dd2
Josh <jproano2023@Elam.co.uk>
2025-04-24 12:40:47 +0100

    we

--- Assets/C# Scripts/Environmeantal/Dialogue/DialogueManager.cs
+++ Assets/C# Scripts/Environmeantal/Dialogue/DialogueManager.cs
@@ -3,58 +3,86 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine;
+using System.Text;
 
 [CreateAssetMenu(menuName = "New Dialogue" , fileName = "New Dialogue")]
 public class DialogueManager : MonoBehaviour
 {
-    [SerializeField] List<Dialogue> choices = new List<Dialogue>();
+    [SerializeField]
+    GameObject dialoguePanel;
 
-    [SerializeField] string optionName;
-    [SerializeField] string dialoguetext;
+    [SerializeField]
+    TextMeshProUGUI dialogueText;
 
-    [SerializeField] GameObject dialoguePanel;
-    [SerializeField] TextMeshProUGUI dialogueText;
-    [SerializeField] GameObject buttonPrefab;
-    [SerializeField] Transform buttonsParent;
-    [SerializeField] GameObject Playercontroller;
-    
+    [SerializeField]
+    GameObject buttonPrefab;
 
-    public string OptionName
+    [SerializeField]
+    Transform buttonsParent;
+
+    [SerializeField]
+    PlayerController playerController;
+
+    public void BeginDialogue(Dialogue dialogue)
     {
-        get { 
-            return optionName;
-        }
-    }
-
-    public List<Dialogue> Choices {
-        get
-        {
-            return choices;
-        }
-    }
-
-    public string DialogueText { 
-      get
-        {
-            return dialoguetext;
-        }
-    }
-
-    public void beginDialogue(Dialogue dialogue)
-    {
-        if(dialogue.Choices.Count == 0)
+        if (dialogue.Choices.Count == 0)
         {
             dialoguePanel.SetActive(false);
 
-            Playercontroller.ToggleMovement(true);
+            playerController.ToggleMovement(true);
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
 
             return;
         }
 
-        Playercontroller.ToggleMovement(false);
+        playerController.ToggleMovement(false);
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+
+        dialoguePanel.SetActive(true);
+
+        ClearChoices();
+        AnimateText(dialogue);
+    }
+
+    void AnimateText(Dialogue dialogue)
+    {
+        IEnumerator TypeText(string text)
+        {
+            StringBuilder textToShow = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                textToShow.Append(text[i]);
+                dialogueText.text = textToShow.ToString();
+
+                yield return new WaitForSeconds(1f / 20f);
+            }
+            ShowChoices(dialogue);
+        }
+
+        StartCoroutine(TypeText(dialogue.DialogueText));
+    }
+
+    void ShowChoices(Dialogue dialogue)
+    {
+        foreach (Dialogue choice in dialogue.Choices)
+        {
+            GameObject newButton = Instantiate(buttonPrefab, buttonsParent);
+
+            newButton.GetComponentInChildren<TextMeshProUGUI>().text = choice.OptionName;
+            newButton.GetComponent<Button>().onClick.AddListener(() => {
+                BeginDialogue(choice);
+            });
+        }
+    }
+
+    void ClearChoices()
+    {
+        foreach (Transform child in buttonsParent)
+        {
+            Destroy(child.gameObject);
+        }
     }
 }
--- Assets/C# Scripts/Environmeantal/Dialogue/DialogueTrigger.cs
+++ Assets/C# Scripts/Environmeantal/Dialogue/DialogueTrigger.cs
@@ -2,41 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class DialogueCharacter
-{
-    public string name;
-    public Sprite icon;
-}
 
-[System.Serializable]
-public class DialogueLine
+public class DialogueTrigger : MonoBehaviour, IInteractable
 {
-    public DialogueCharacter character;
-    [TextArea(3, 10)]
-    public string line;
-}
 
-[System.Serializable]
-public class Dialogue
-{
-    public List<DialogueLine> dialogueLines = new List<DialogueLine>();
-}
-
-public class DialogueTrigger : MonoBehaviour
-{
-    public Dialogue dialogue;
-
-    public void TriggerDialogue()
+    [SerializeField] DialogueManager dialogueManager;
+    [SerializeField] Dialogue dialogue;
+    
+    public string GetDescription()
     {
-        DialogueManager.Instance.StartDialogue(dialogue);
+        return "Talk to Bob";
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void Interact()
     {
-        if (collision.tag == "Player")
-        {
-            TriggerDialogue();
-        }
+        dialogueManager.BeginDialogue(dialogue);
     }
 }
